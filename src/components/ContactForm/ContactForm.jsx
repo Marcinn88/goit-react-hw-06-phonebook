10f192b779f0d0b4cbebc88c901d2c631dffa176
Marcinn88 <111724779+Marcinn88@users.noreply.github.com>
2023-07-17 18:37:11 +0200

    done

---
+++ src/components/ContactForm/ContactForm.jsx
@@ -0,0 +1,82 @@
+import React, { useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { addContact } from '../../redux/contactsSlice';
+import { Form, Input, Text, Button } from './ContactForm.styled';
+
+function ContactForm() {
+  const dispatch = useDispatch();
+  const contacts = useSelector(state => state.contacts);
+
+  const [name, setName] = useState('');
+  const [number, setNumber] = useState('');
+
+  const handleNameChange = event => {
+    setName(event.target.value);
+  };
+
+  const handleNumberChange = event => {
+    setNumber(event.target.value);
+  };
+
+  const handleSubmit = event => {
+    event.preventDefault();
+
+    if (name.trim() === '' || number.trim() === '') {
+      return;
+    }
+
+    const isContactExist = contacts.find(
+      contact => contact.name.toLowerCase() === name.toLowerCase()
+    );
+
+    if (isContactExist) {
+      alert(`Contact with name ${name} already exists!`);
+      return;
+    }
+
+    const isNumberExist = contacts.find(
+      contact => contact.number.replace(/\D/g, '') === number.replace(/\D/g, '')
+    );
+
+    if (isNumberExist) {
+      alert(`Number ${number} is already in contacts!`);
+      return;
+    }
+
+    dispatch(addContact(name, number));
+    setName('');
+    setNumber('');
+  };
+
+  return (
+    <Form onSubmit={handleSubmit}>
+      <Text>Name</Text>
+      <Input
+        type="text"
+        name="name"
+        pattern="^[a-zA-Zа-яА-Я]+([' \-][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*$"
+        title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+        required
+        value={name}
+        onChange={handleNameChange}
+      />
+
+      <Text>Number</Text>
+      <Input
+        type="tel"
+        name="number"
+        pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+        title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+        required
+        value={number}
+        onChange={handleNumberChange}
+      />
+
+      <Button type="submit">
+        Add
+      </Button>
+    </Form>
+  );
+}
+
+export default ContactForm;
